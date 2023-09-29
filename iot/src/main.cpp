906fe2c401a956da3af6f5fac30e9474b71303bf
Johnny Nguyen <cuongdn2001@gmail.com>
2023-09-29 20:43:52 +1000

    Update IoT

--- iot/src/main.cpp
+++ iot/src/main.cpp
@@ -3,27 +3,28 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#include "rfid.h"
-
-#include <ESP8266WiFi.h>
-#include <WiFiManager.h>
-
-#include <ESP8266HTTPClient.h>
+#include <WiFi.h>
 #include <ArduinoJson.h>
+#include <HTTPClient.h>
 
-constexpr uint8_t RST_PIN = 16;        // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = 15;         // Configurable, see typical pin layout above
+constexpr uint8_t RST_PIN = 22;        // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 5;         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 String tagID = "";
 
 boolean getUID() {
+  Serial.println("Getting UID");
   // Getting ready for reading Tags
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {   //If a new tag is placed close to the RFID reader, continue
+  if (!mfrc522.PICC_IsNewCardPresent()) {   //If a new tag is placed close to the RFID reader, continue
+    // delay(2000);
+    // Serial.println("No new card");
     return false;
   }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {     //When a tag is placed, get UID and continue
+  if (!mfrc522.PICC_ReadCardSerial()) {     //When a tag is placed, get UID and continue
+    // delay(2000);
+    // Serial.println("No card serial");
     return false;
   }
 
@@ -33,45 +34,28 @@ boolean getUID() {
   }
   tagID.toUpperCase();
 
-  
+  // Serial.println("Tag ID: " + tagID);
 
   mfrc522.PICC_HaltA(); // Stop reading
   return true;
 }
 
-String SERVER_ADDRESS = "192.168.1.122";
-String SERVER_PORT = "3000";
+const char * SERVER_ADDRESS = "172.20.10.9";
+const uint16_t SERVER_PORT = 3000;
 String postInboundRoute = "/iot/inbound";
-
-void getRequest() {
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected to WiFi - Strength" + String(WiFi.RSSI()));
-    WiFiClient client;
-
-    // Send GET request
-    if (client.connect(String(SERVER_ADDRESS), 3000)) {
-      client.println("GET " + postInboundRoute + " HTTP/1.1");
-      client.println("Host: " + String(SERVER_ADDRESS));
-      client.println("Connection: close");
-      client.println();
-      while (client.connected()) {
-          String line = client.readStringUntil('\n');
-          Serial.println(line);
-      }
-      client.stop();
-    } else {
-      Serial.println("GET Connection failed.");
-    }
-  }
-}
 
 void postTag(String tagID) {
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("Posting tag - Wifi Strength: " + String(WiFi.RSSI()));
     WiFiClient client;
 
+    if (!client.connect(SERVER_ADDRESS, SERVER_PORT)) {
+      Serial.println("Connection failed.");
+      return;
+    }
+
     // Send POST request
-    if (client.connect( String(SERVER_ADDRESS), 3000)) {
+    if (client.connect(SERVER_ADDRESS, SERVER_PORT)) { // Use c_str() to convert String to const char*
       StaticJsonDocument<200> jsonDoc; // Adjust the size as needed
       jsonDoc["sensor"] = "ESP8266";
       jsonDoc["role"] = "inbound";
@@ -100,17 +84,15 @@ void postTag(String tagID) {
     } else {
       Serial.println("POST Tag failed. Tag: " + tagID);
     }
-
-
   }
 }
 
 
 // Replace with your network credentials (STATION)
-const char* ssid = "WiFi-DFAE-5G-pro-5G";
-const char* password = "18190793";
+const char* ssid = "SaeForWork";
+const char* password = "08102001";
 void initWifi() {
-  WiFi.mode(WIFI_STA);
+  // WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
@@ -119,31 +101,35 @@ void initWifi() {
   }
   Serial.println("WiFi connected");
   Serial.println(WiFi.localIP());
-  }
+  Serial.println(WiFi.SSID());
+}
 
 void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
+	Serial.begin(115200);		// Initialize serial communications with the PC
 	while (!Serial);		  // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			    // Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
 
-  WiFiManager wifiManager;
-  if (!wifiManager.autoConnect()) {
-    Serial.println("Failed to connect wifi and hit timeout.");
-    // Reset and try again, or do something else
-    ESP.reset();
-    delay(1000);
-  }
-  Serial.println("Done Setup!");
-  Serial.println("Connected to: " + WiFi.SSID());
+  initWifi();
+
+  // WiFiManager wifiManager;
+  // if (!wifiManager.autoConnect()) {
+  //   Serial.println("Failed to connect wifi and hit timeout.");
+  //   // Reset and try again, or do something else
+  //   ESP.reset();
+  //   delay(1000);
+  // }
+  // Serial.println("Done Setup!");
+  // Serial.println("Connected to: " + WiFi.SSID());
 }
 
 void loop() {
   
-  while (getUID()) 
+  if (getUID()) 
   {
     Serial.println("Tag " + tagID + " detected");
     // getRequest();
     postTag(tagID);
+    delay(2000);
   }
 }
