fa4fe5903d5948b1af79af7ea6ab2031389e5dfa
Johnny Nguyen <cuongdn2001@gmail.com>
2023-10-09 23:43:56 +1100

    Update position for authenticateJWT

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,9 @@ app.get("/", (req, res) => {
 
 app.use(process.env.ENDPOINT, authRouter);
 
-app.use(authenticateJWT);
 app.use(process.env.ENDPOINT, warehouseRouter);
+
+app.use(authenticateJWT);
 app.use(process.env.ENDPOINT, shelfRouter);
 app.use(process.env.ENDPOINT, productRouter);
 app.use(process.env.ENDPOINT, parcelRouter);
