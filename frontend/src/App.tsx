b47f125d8aa984a969a4fe6aa11309e0d5164502
Johnny Nguyen <cuongdn2001@gmail.com>
2023-10-16 14:04:57 +1100

    Add dashboard and login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,56 +12,57 @@ import {
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Layout } from "antd";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  const token = localStorage.getItem("token");
+  const [token, setToken] = useState<string | null>() 
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token'))
+  }, [])
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/"
-          element={
-            token ? (
-              <Layout style={{ minHeight: "100vh" }}>
-                <Sidebar />
-                <Layout>
-                  <Header style={{ padding: 0, background: "#4796bd" }} />
-                  <Content style={{ margin: "0 16px" }}>
-                    <Routes>
-                      <Route path="/" element={<Dashboard />} />
-                      <Route path="/parcels" element={<Parcels />} />
-                      <Route path="/items" element={<Items />} />
-                      <Route path="/inbound" element={<InboundPage />} />
-                      <Route path="/outlet" element={<OutletPage />} />
-                      <Route
-                        path="/outlet/new-order"
-                        element={<NewOrderPage />}
-                      />
-                    </Routes>
-                  </Content>
-                  <Footer
-                    style={{
-                      backgroundColor: "#4796bd",
-                      textAlign: "center",
-                      color: "#fff",
-                      fontSize: "1.15em",
-                    }}
-                  >
-                    IMS © 2023 Created by DC
-                  </Footer>
-                </Layout>
-              </Layout>
-            ) : (
-              <Navigate to="/login" replace={true} />
-            )
-          }
-        />
-      </Routes>
+      {!token ?
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/*" element={<Navigate to="/login" />} />
+        </Routes>
+      :
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sidebar />
+        <Layout>
+          <Header style={{ padding: 0, background: "#4796bd" }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/parcels" element={<Parcels />} />
+              <Route path="/items" element={<Items />} />
+              <Route path="/inbound" element={<InboundPage />} />
+              <Route path="/outlet" element={<OutletPage />} />
+              <Route
+                path="/outlet/new-order"
+                element={<NewOrderPage />}
+              />
+              <Route path="/*" element={<Navigate to="/" />} />
+            </Routes>
+          </Content>
+          <Footer
+            style={{
+              backgroundColor: "#4796bd",
+              textAlign: "center",
+              color: "#fff",
+              fontSize: "1.15em",
+            }}
+          >
+            IMS © 2023 Created by DC
+          </Footer>
+        </Layout>
+      </Layout>
+      }
     </BrowserRouter>
   );
 };
