e1c787952a467fcf9e57655539bd6d441ad4804e
Nam Dang <nathandang73@gmail.com>
2023-11-04 22:22:18 +1100

    Add datetimecreated and datetimeupdated for user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,26 +1,42 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fullname: String,
-  username: String,
-  password: String, // Hashed password
-  role: {
-    type: String,
-    enum: ["manager", "staff", "outlet", "supplier"],
-    default: "worker",
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: String,
+    username: String,
+    password: String, // Hashed password
+    role: {
+      type: String,
+      enum: ["manager", "staff", "outlet", "supplier"],
+      default: "worker",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    abn: String,
+    address: String,
+    phone: String,
+    email: String,
+    warehouses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Warehouse" }],
+    datetimecreated: {
+      type: Date,
+      required: true,
+    },
+    datetimeupdated: {
+      type: Date,
+      required: true,
+    },
   },
-  status: {
-    type: String,
-    enum: ["pending", "accepted", "rejected"],
-    default: "pending",
-  },
-  abn: String,
-  address: String,
-  phone: String,
-  email: String,
-  warehouses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Warehouse" }],
-});
+  {
+    timestamps: {
+      createdAt: "datetimecreated",
+      updatedAt: "datetimeupdated",
+    },
+  }
+);
 
 const User = mongoose.model.User || mongoose.model("User", userSchema);
 
